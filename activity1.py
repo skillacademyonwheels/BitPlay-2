ee0fd7de8f93c4d40a8b3da23a0ff77a20e1c454
skillacademyonwheels <advelestica.design@gmail.com>
2026-03-21 19:32:42 +0530

    v1

---
+++ activity1.py
@@ -0,0 +1,15 @@
+# Program to check if user input numbers are equal without using any comparison operator. 
+ 
+def checkIfSame(number1, number2):
+ 
+# User XOR operator as a^a is always 0 
+ if ((number1 ^ number2) != 0):
+    print("Numbers are not equal")
+ else:
+    print("Both numbers are equal")
+ 
+# Taking input
+number1 = int(input("Enter first number to compare : "))
+number2 = int(input("Enter second number to compare : "))
+ 
+checkIfSame(number1, number2)
